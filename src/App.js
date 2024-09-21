70b20e4b0c5041c2ca45632bbaef4e2bba04d6e1
Karolina <karolina.kotwica@powercoders.org>
2024-09-21 17:53:06 +0200

    Add screen-root-container class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Provider } from 'react-redux';
 import store from './store/store';
-import Nav from './components/Nav';
-import Header from './components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './main.css';
 import Home from './components/Home';
@@ -11,8 +9,6 @@ function App() {
   return (
     <div className="App">
         <Provider store={store}>
-            <Nav />
-            <Header />
             <Routes>
               <Route path="/" element={<Home />} />
               {/* <Route path="/category/:id" element={<Category />} /> */}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Main from './Main';
 import GroupCategory from './Category';
+import Nav from './Nav';
+import Header from './Header';
 
 const Home = () => {
   return (
-    <div>
-      <Main />
-      <GroupCategory />
+    <div className="screen-root-container home-container">
+        <Nav />
+        <Header />
+        <Main />
+        <GroupCategory />
     </div>
   );
 };
